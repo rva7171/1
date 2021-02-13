f28b7bbb1b89bd5096a1f940c63ed9c586c7e80f
Vladimir <rva7171@gmail.com>
2021-02-13 21:10:11 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//alert("Hello");
+/* const result = confirm("Are you htre?");
+console.log(result); */
+/* const answer = prompt("Вам есть 18?", "18");
+console.log(typeof answer); */
+
+let incr = 10,
+   decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
