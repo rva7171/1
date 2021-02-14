42e9f732c2c29381ee767787aef3ff85e35308eb
Vladimir <rva7171@gmail.com>
2021-02-14 17:31:57 +0300

    циклы for while - do while

--- js/script.js
+++ js/script.js
@@ -9,12 +9,55 @@ const persanalMovieDB = {
    privat: false,
 };
 
-const a = prompt("Один из последних просмотренных фильмов", ""),
-   b = prompt("На сколько оцените его?", ""),
-   c = prompt("Один из последних просмотренных фильмов", ""),
-   d = prompt("На сколько оцените его?", "");
+for (let i = 0; i < 2; i++) {
+   const a = prompt("Один из последних просмотренных фильмов", ""),
+      b = prompt("На сколько оцените его?", "");
 
-persanalMovieDB.movies[a] = b;
-persanalMovieDB.movies[c] = d;
+   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+      persanalMovieDB.movies[a] = b;
+      console.log("done");
+   } else {
+      console.log("error");
+      i--;
+   }
+}
+/* let i = 0;
+while (i < 2) {
+   const a = prompt("Один из последних просмотренных фильмов", ""),
+      b = prompt("На сколько оцените его?", "");
+
+   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+      persanalMovieDB.movies[a] = b;
+      console.log("done");
+   } else {
+      console.log("error");
+      i--;
+   }
+   i++;
+}
+
+do {
+   const a = prompt("Один из последних просмотренных фильмов", ""),
+      b = prompt("На сколько оцените его?", "");
+
+   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+      persanalMovieDB.movies[a] = b;
+      console.log("done");
+   } else {
+      console.log("error");
+      i--;
+   }
+   i++;
+} while (i < 3); */
+
+if (persanalMovieDB.count < 10) {
+   console.log("Просмотрено довольно мало фильмов");
+} else if (persanalMovieDB.count >= 10 && persanalMovieDB.count < 30) {
+   console.log("Вы классический зритель");
+} else if (persanalMovieDB.count >= 30) {
+   console.log("Вы киноман");
+} else {
+   console.log("Произошла ошибка");
+}
 
 console.log(persanalMovieDB);
